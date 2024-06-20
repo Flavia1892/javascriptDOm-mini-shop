00c0ad2deb54e4f1d2a7d9c1c0d75ee0ca1ed33b
Flavia Bobeica <bobeicaflavia8@gmail.com>
2024-06-20 19:13:28 +0300

    firstFunctions

--- scripts.js
+++ scripts.js
@@ -6,6 +6,16 @@ let products = [
   { id: 5, name: "Product 5", price: 30, quantity: 3 },
 ];
 
+let counterQuantity = [
+  { id: 1, name: "Product 1", price: 10, quantitycount: 0 },
+  { id: 2, name: "Product 2", price: 20, quantitycount: 0 },
+  { id: 3, name: "Product 3", price: 30, quantitycount: 0 },
+  { id: 4, name: "Product 4", price: 30, quantitycount: 0 },
+  { id: 5, name: "Product 5", price: 30, quantitycount: 0 },
+];
+
+const productsCopy = [...products];
+
 let cart = [];
 
 function renderProducts() {
@@ -50,6 +60,7 @@ function renderCart() {
   const cartList = document.getElementById("cart-list");
 
   // ii dictez ce sa contina
+
   cartList.innerHTML =
     // folosind cart object, iterand prin el cu map
     cart
@@ -57,9 +68,14 @@ function renderCart() {
         // plus fiecarui element ii spun cum sa arate cu niste HTML si bootstrap classes
         (item) => `
           <li class="list-group-item d-flex justify-content-between align-items-center">
-              ${item.name} - $${item.price}
-              <button class="btn btn-danger btn-sm remove-from-cart" data-id="${item.id}">Remove</button>
+              ${item.name} - $${item.price}                  
+              <button class="btn btn-danger btn-sm remove-from-cart" data-id="${
+                item.id
+              }">Remove</button>
           </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center" id='quant'> Quantity: ${
+            counterQuantity[item.id - 1].quantitycount
+          }</li>
       `
       )
       // iar la final facem join, ca din array (prin cart.map)
@@ -80,6 +96,14 @@ function renderCart() {
   });
 }
 
+function checkQuantityOfProductsList() {
+  return counterQuantity.every(checkQuantity(obj));
+
+  function checkQuantity(obj) {
+    return obj.quantitycount <= 1;
+  }
+}
+
 //Calculate the quantity of products
 function calculateQuantity(productId) {
   const product = products.find((p) => p.id === productId);
@@ -89,7 +113,10 @@ function calculateQuantity(productId) {
     outOfStock[productId - 1].innerHTML = `OutOfStock`;
     return;
   } else {
-    cart.push(product);
+    if (checkCart(productId)) cart.push(product);
+
+    const counter = counterQuantity.find((p) => p.id === productId);
+    counter.quantitycount++;
   }
 
   product.quantity--;
@@ -101,6 +128,12 @@ function calculateQuantity(productId) {
     outOfStock[productId - 1].innerHTML = `OutOfStock`;
     return;
   }
+}
+
+function checkCart(productId) {
+  for (let key in cart) if (cart[key].id === productId) return false;
+
+  return true;
 }
 
 function addToCart(productId) {
@@ -119,12 +152,31 @@ function addToCart(productId) {
 function removeFromCart(productId) {
   // filtreaza-mi tot ce e diferit ce input "productId"
   // obtin un array fara ce am pasat in input
-  cart = cart.filter((item) => item.id !== productId);
+  // cart = cart.filter((item) => item.id !== productId);
+
+  if (counterQuantity[productId - 1].quantitycount == 1)
+    cart = cart.filter((item) => item.id !== productId);
+  else {
+
+    let prodDecrease = document.querySelectorAll("[id='quant']");
+    prodDecrease.innerHTML = `Quantity: ${
+      counterQuantity[productId - 1].quantitycount--
+    }`;
+  }
+
+  counterQuantity[productId - 1].quantitycount;
+  products[productId - 1].quantity++;
+  const cantProdu = document.querySelectorAll("[id='quantity']");
+  cantProdu[productId - 1].innerHTML = `Quantity: ${
+    products[productId - 1].quantity
+  }`;
 
   // update la datele afisate
   const totalProducts = document.querySelector("h2");
   totalProducts.innerHTML = `<h2>Total produse: ${cart.length}</h2>`;
-  renderCart();
+  const outOfStock = document.querySelectorAll("[id='addToCart']");
+  outOfStock[productId - 1].innerHTML = `Add to Cart`;
+  renderCart(productId);
 }
 
 function resetQuantity() {
@@ -146,10 +198,32 @@ function resetQuantity() {
     outOfStock[i].innerHTML = `Add to Cart`;
   }
 }
+
+function resetCart() {
+  const cartList = document.getElementById("cart-list");
+  cartList.innerHTML =
+    // folosind cart object, iterand prin el cu map
+    cart
+      .map(
+        // plus fiecarui element ii spun cum sa arate cu niste HTML si bootstrap classes
+        (item) => `
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+              ${item.name} - $${item.price}                  
+              <button class="btn btn-danger btn-sm remove-from-cart" data-id="${item.id}">Remove</button>
+          </li>
+               `
+      )
+      // iar la final facem join, ca din array (prin cart.map)
+      // sa devina un string, stringul fiind cel asteptat in .innerHTML (ceva de genul: "<li>test</li><li>test</li><li>test</li>")
+      .join("");
+}
+
+function resetQuant() {
+  for (let key in counterQuantity) counterQuantity[key].quantitycount = 0;
+}
+
 function checkout() {
   let total = 0;
-
-  console.log(cart);
 
   if (cart.length === 0) {
     {
@@ -159,8 +233,14 @@ function checkout() {
       resetQuantity();
       alert("Cart is empty!");
     }
-    return;
-  } else total = cart.reduce((total, arr) => total + arr.price, 0);
+    // return;
+  } else
+    for (let key in counterQuantity)
+      total =
+        total + counterQuantity[key].price * counterQuantity[key].quantitycount;
+
+  resetQuant();
+  console.log(counterQuantity);
 
   // afiseaza mesajul
   const butonel = document.getElementById("checkout-btn");
@@ -170,11 +250,13 @@ function checkout() {
 
   // acum ca a dat checkout si "a cumparat"
   // golim cartul pentru alte cumparaturi :)
+
   cart.length = 0; // Clear the cart
+  cart = [];
 
   // si facem iar update
 
-  renderCart();
+  resetCart();
 }
 
 document.getElementById("checkout-btn").addEventListener("click", checkout);
